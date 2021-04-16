34c221e1bd6c4302803bf148f27eab0b3e575ac6
priyobasu <priyobasu1806@gmail.com>
2021-04-16 19:30:30 +0530

    Coding class

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    // coding class with nosshy malhotra
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
